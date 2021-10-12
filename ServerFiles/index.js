a9937e48ae239c1726a90a8405afd2893659287b
Damilare Odulesi <57369837+jossydee1@users.noreply.github.com>
2021-10-12 13:01:52 +0100

    Add files via upload

--- ServerFiles/index.js
+++ ServerFiles/index.js
@@ -1,1 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
 
+const db = mysql.createPool({
+	host: "localhost",
+	user: "root",
+	database: "vms",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res) => {
+	// res.send("Helllooooooo");
+	const sqlSelect = "SELECT * FROM visitors";
+	db.query(sqlSelect, (err, result) => {
+		console.log(result);
+		res.send(result);
+	});
+});
+
+app.post("/api/insert", (req, res) => {
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+
+	const sqlInsert = "INSERT INTO visitors (firstName, lastName) VALUES (?,?)";
+	db.query(sqlInsert, [firstName, lastName], (err, result) => {
+		res.send(result);
+		console.log(err);
+	});
+});
+
+app.listen(3001, () => {
+	console.log("We are on the right track 3001");
+});
